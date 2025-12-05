e763ee1f1c1af4e632683d406fc738bb0116083a
Celeste Amador <celesteamador@proton.me>
2025-12-05 14:09:42 -0500

    functions

--- functions/funciones.cpp
+++ functions/funciones.cpp
@@ -6,49 +6,49 @@ using namespace std;
 
 int main()
 {
-    float value, operation, result;
-    string func;
-    clock_t t;
+    float value, operation, result; //Definiendo variables
+    string func; //String para el cambio de nombre al mostrar el tiempo de operacion de acuerdo a la funcion seleccionada.
+    clock_t t; //Declaracion de la variable t para almacenar la hora del reloj.
 
-    t=clock();
-    cout<<"Enter a number: ";
-    cin>> value;
-    cout<<"Select the function to perform:\n 1.Sine\n 2.Cosine\n 3.Tangent\n Function selected: ";
-    cin>> operation;
+    t=clock(); //Hora de inicio de programa
+    cout<<"Enter a number: "; //Usuario ingresa un numero
+    cin>> value; //El numero se guarda en la variable value
+    cout<<"Select the function to perform:\n 1.Sine\n 2.Cosine\n 3.Tangent\n Function selected: ";  //Menu de seleccion
+    cin>> operation; //Se guarda la funcion seleccionada en la variable operation
 
-    if(operation == 1) //Sine
+    if(operation == 1) //Sine == Si se ingresa 1
     {
         func = "sine";
-        result = sin(value);
-        cout<<"The sine of "<<value<<" is "<<result<<endl;
+        result = sin(value); //funcion de seno
+        cout<<"The sine of "<<value<<" is "<<result<<endl; //Impresion de resultado
     }
     else
     {
-        if(operation == 2) //Cosine
+        if(operation == 2) //Cosine == si se ingresa 2
         {
             func = "cosine";
-            result = cos(value);
-            cout<<"The cosine of "<<value<<" is "<<result<<endl;
+            result = cos(value); //funcion de coseno
+            cout<<"The cosine of "<<value<<" is "<<result<<endl; //Impresion de resultado
         }
         else
         {
-            func = "tangent";
-            if(operation == 3) //Tangent
+            if(operation == 3) //Tangent == si se ingresa 3
             {
-                result = tan(value);
-                cout<<"The tangent of "<<value<<" is "<<result<<endl;
+                func = "tangent";
+                result = tan(value); //funcion de tangente
+                cout<<"The tangent of "<<value<<" is "<<result<<endl; //Impresion de resultado
             }
-            else
+            else //Si se ingresa cualquier otro numero
             {
                 func = "program";
-                cout<<"Invalid option\n";
+                cout<<"Invalid option\n"; //Opcion invalida
             }
 
         }
 
     }
-    t=clock()-t;
-    cout<<"The execution time of the "<<func<<" was: "<<float(t)/CLOCKS_PER_SEC<<" seconds\n";
+    t=clock()-t; //Tiempo final de ejecucion
+    cout<<"The execution time of the "<<func<<" was: "<<float(t)/CLOCKS_PER_SEC<<" seconds\n"; //Impresion de tiempo de ejecucion
 
 
 
